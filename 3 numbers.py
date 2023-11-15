d2d0624966400802e6efdadf6908d3d8f1a45a1b
SheikhaAlhadei <s56J1711447@outlook.com>
2023-11-15 15:33:51 +0400

    commmit

---
+++ 3 numbers.py
@@ -0,0 +1,13 @@
+a=int(input("enter number1: "))
+b=int(input("enter number2: "))
+c=int(input("enter number3: "))
+
+if(a>=b and a>=c):
+    print(str(a)+" is the largest")
+elif(b>=a and b>=c):
+    print(str(b)+" is the largest")
+else:
+    print("")
+
+   
+    
