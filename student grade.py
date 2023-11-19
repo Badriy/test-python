2ec5716af4982d128443f6adccf5dce0bc431bfc
SheikhaAlhadei <s56J1711447@outlook.com>
2023-11-19 13:40:13 +0400

    commit

--- student grade.py
+++ student grade.py
@@ -1,7 +1,10 @@
-sum2 = 0
-stu = int(input("Number of stuents:"))
-for i in range(1,stu+1):
-    grade = float(input("Enter student grade:"))
-    sum2 +=grade
-avg = sum2/stu
-print(avg)
+stu = int(input("Number of students:"))
+def grade_avg (x):
+    sum2 = 0
+    for i in range(1,x+1):
+        grade = float(input("Enter student grade:"))
+        sum2 +=grade
+    avg = sum2/x
+    print(avg)
+
+grade_avg(stu)
